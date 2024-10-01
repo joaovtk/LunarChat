e67a99c46062163d9e6d515cf2deb92dc1ad149c
amandaversiani <amandaversianni@gmail.com>
2024-10-01 15:59:15 -0300

    Initial Project

---
+++ src/test/java/app/lunarchat/lunarchat/LunarchatApplicationTests.java
@@ -0,0 +1,13 @@
+package app.lunarchat.lunarchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunarchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
