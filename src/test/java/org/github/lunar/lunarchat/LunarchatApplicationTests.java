af1f8cdca1c1a15a28ad4f72c591bbbd421cba28
João Vitor Soares dos Reis <101532024+joaovtk@users.noreply.github.com>
2024-10-21 15:35:36 -0300

    Add files via upload

---
+++ src/test/java/org/github/lunar/lunarchat/LunarchatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.lunar.lunarchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunarchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
